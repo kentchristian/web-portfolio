b9142db94191e877647807dcdc11f335365db44b
keysii <kentchristiancagadas@gmail.com>
2026-01-22 22:09:08 +0800

    feat: dashboard layout

--- src/portfolio/pages/dashboard.tsx
+++ src/portfolio/pages/dashboard.tsx
@@ -1,5 +1,15 @@
 const Dashboard = () => {
-  return <h1>dashboard</h1>;
+  return (
+    <div className="grid grid-rows-3 gap-2 px-20 my-5">
+      <div className="cols-pan-1 border">Welcome message here </div>
+      <div className="cols-pan-1 border">Overview Cards here</div>
+
+      <div className="cols-pan-1 border grid grid-cols-2">
+        <div className="cols-pan-1 border">Metrics Here</div>
+        <div className="cols-pan-1 border">Skills Description</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
